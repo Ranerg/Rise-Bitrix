b306d75e6bb12b77a35f3e07200df575802a0981
Roman Anufriev <Ranerg@yandex.ru>
2015-03-05 14:03:48 +0800

    Подключение вертски

--- footer.php
+++ footer.php
@@ -1,1 +1,33 @@
+		</div>
+		<!-- end of center layout -->
 
+		
+		<div id="right">
+			<div class="caption"><p>Latest Posts</p></div>
+			<div class="box">
+				<ul>
+					<li><a href="#">Genelia Photos</a></li>
+					<li><a href="#">Genelia Ready</a></li>
+					<li><a href="#">Trisha Private</a></li>
+					<li><a href="#">Actress</a></li>
+					<li><a href="#">Bollywood</a></li>
+					<li><a href="#">Phasellus vel leo</a></li>
+					<li><a href="#">Nam gravida nulla </a></li>
+					<li><a href="#">Curabitur sollicit</a></li>
+					<li><a href="#">Suspendisse </a></li>
+					<li><a href="#">Maecenas libero</a></li>
+					<li><a href="#">Curabitur sodales</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<div id="footer">
+			<div id="fl">
+				<p>&copy; Copyright 2007 Internet Studio<br />
+				Design: <a href="http://gendesigns.blogspot.com/">Gen</a> &middot; Sponsored by <a href="http://gendesigns.com/">Gen</a> &middot;</p>
+			<div style="display:none;"><a href="http://free-templates.ru">áåñïëàòíûå øàáëîíû</a></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,46 @@
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<?$APPLICATION->SetTemplateCSS("/template_style.css");?>
+	<?$APPLICATION->ShowHead();?>
+	<title>Studio 5</title>
+</head>
+<body>
+	<div id="content">
+		<div id="logo">
+			<p>equality,liberty,democracy</p>
+			<h1>logo or slogan</h1>
+		</div>
+		<ul id="menu">
+			<li><a class="current" href="#">Home</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Refrences</a></li>
+			<li><a href="#">Career</a></li>
+			<li><a href="#">About</a></li>
+		</ul>
+		
+		<div class="clear"><br /></div>
+		
+		
+		
+		<div id="left">
+			<div class="caption"><p>Latest Posts</p></div>
+			<div class="box">
+				<ul>
+					<li><a href="#">Genelia Photos</a></li>
+					<li><a href="#">Genelia Ready</a></li>
+					<li><a href="#">Trisha Private</a></li>
+					<li><a href="#">Actress</a></li>
+					<li><a href="#">Bollywood</a></li>
+					<li><a href="#">Phasellus vel leo</a></li>
+					<li><a href="#">Nam gravida nulla </a></li>
+					<li><a href="#">Curabitur sollicit</a></li>
+					<li><a href="#">Suspendisse </a></li>
+					<li><a href="#">Maecenas libero</a></li>
+					<li><a href="#">Curabitur sodales</a></li>
+				</ul>
+			</div>
+		</div>
+		
+		<!-- start of center layout -->
+		<div id="center">
